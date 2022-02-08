69c7f2e7dea5a62670ba5a5e65bc4b7397358f07
chenshuhong <chenshuhong@mq-ai.cn>
2022-02-08 17:19:23 +0800

    优化下代码

--- examples/Get-Printer-Attributes.js
+++ examples/Get-Printer-Attributes.js
@@ -1,6 +1,6 @@
 var ipp = require('./../ipp');
 
-var printer = ipp.Printer("http://cp02.local.:631/ipp/printer");
+var printer = ipp.Printer("http://172.16.1.60:631/ipp/printer");
 printer.execute("Get-Printer-Attributes", null, function(err, res){
 	console.log(res);
 });
--- examples/wx/Get-Printer-Attributes.js
+++ examples/wx/Get-Printer-Attributes.js
@@ -1,5 +1,5 @@
 var ipp = require('./../../ipp');
-global.wx = function(){}
+//global.wx = function(){}
 var printer = ipp.Printer("http://172.16.1.60:631/ipp/printer");
 printer.execute("Get-Printer-Attributes", null, function(err, res){
 	console.log(res);
