f56b3544c4e4666202d7abb610685f1cfd54a94e
chenshuhong <chenshuhong@mq-ai.cn>
2022-04-06 20:42:10 +0800

    增加小程序兼容

--- examples/Get-Printer-Attributes.js
+++ examples/Get-Printer-Attributes.js
@@ -1,6 +1,6 @@
 var ipp = require('./../ipp');
 
-var printer = ipp.Printer("http://172.16.1.19:631/ipp/print");
+var printer = ipp.Printer("http://172.16.1.60:631/ipp/print");
 printer.execute("Get-Printer-Attributes", null, function(err, res){
 	console.log(res);
 });
